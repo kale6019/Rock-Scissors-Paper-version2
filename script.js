a57875eb71f9281800ab450030992122f108f4f3
kale6019 <kale6019@gmail.com>
2023-09-21 19:28:16 +0800

    changed computer selection function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let playerScore = 0
 let computerScore = 0
-       
+
 function playRound(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
+        playerScore++
+        computerScore++
         return 'Tie!';
     }
     else if(playerSelection == 'rock' && computerSelection == 'paper'){
@@ -36,34 +38,37 @@ function playRound(playerSelection, computerSelection){
    
 }
 
+function game (playerScore, computerScore){
+    if (playerScore > computerScore){
+        return 'Player wins!!';
+    }
+    else if (playerScore < computerScore){
+        return 'Player Loses!!';
+    }
+    
+    else {return 'Its a tie!!';
+    }
+    }
+
 for (let i = 0; i < 5; i++){
-let playerSelection = prompt('Pick!').toLowerCase();
-let computerSelection = Math.random()
-if (computerSelection < 0.33){
-    computerSelection = 'rock';
+    let playerSelection = prompt('Pick!').toLowerCase();
+    let computerSelection = Math.floor(Math.random()*3)
+    function choice(){
+        if (computerSelection === 0){
+            computerSelection = 'rock';
+        }
+    
+        else if(computerSelection === 1){
+            computerSelection = 'paper';
+        }
+    
+        else {computerSelection = 'scissors';
+    }}
+    choice();
+    console.log (playerSelection)
+    console.log (computerSelection)
+    console.log (playRound(playerSelection, computerSelection))
+    console.log (playerScore)
+    console.log (computerScore)
 }
-else{
-    if(computerSelection < 0.66){
-        computerSelection = 'paper';
-    }
-    else{computerSelection = 'scissors';
-}
-}
-console.log('player selection =', playerSelection);
-console.log('computer selection =', computerSelection);
-console.log(playRound(playerSelection, computerSelection));
-console.log('Player Score =',playerScore);
-console.log('Computer Score =',computerScore);}
-
-function score(playerScore, computerScore){
-    if(playerScore == computerScore){
-        return 'The game is a draw!!!';
-
-    }
-    else if(playerScore > computerScore){
-        return 'You win the game!!!'
-    }
-    else{ return 'You lose the game!!!'}
-}
-
-console.log(score())
+console.log(game(playerScore, computerScore))
